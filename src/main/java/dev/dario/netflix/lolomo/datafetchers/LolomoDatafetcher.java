b666cd13231109aa61cbc16ed4918fe974aaf762
Carlos D <dario20049@gmail.com>
2024-11-18 19:54:21 +0900

    init

--- src/main/java/dev/dario/netflix/lolomo/datafetchers/LolomoDatafetcher.java
+++ src/main/java/dev/dario/netflix/lolomo/datafetchers/LolomoDatafetcher.java
@@ -1,4 +1,26 @@
 package dev.dario.netflix.lolomo.datafetchers;
 
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+import dev.dario.netflix.lolomo.codegen.types.Show;
+import dev.dario.netflix.lolomo.codegen.types.ShowCategory;
+
+import java.util.List;
+
+@DgsComponent
 public class LolomoDatafetcher {
+
+    @DgsQuery
+    public List<ShowCategory> lolomo(){
+        return List.of(ShowCategory.newBuilder()
+                .name("Top 10")
+                .shows(
+                        List.of(Show.newBuilder().title("Show 1").build())
+                ).build());
+    }
+
+
+
+
+
 }
